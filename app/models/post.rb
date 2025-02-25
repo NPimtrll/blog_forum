d194ecc87eb9342d93b93e2cf9347a7456d02d04
Nannapat <NannapatSrimartpirom@gmail.com>
2025-02-25 15:36:39 +0700

    เพิ่ม catagory viewกระทู้ tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_24_064859) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_25_081142) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.string "user"
@@ -29,12 +35,37 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_24_064859) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_categories_on_category_id"
+    t.index ["post_id"], name: "index_post_categories_on_post_id"
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "views", default: 0, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,5 +83,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_24_064859) do
   add_foreign_key "comments", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "post_categories", "categories"
+  add_foreign_key "post_categories", "posts"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "users"
 end
