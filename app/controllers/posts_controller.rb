7b45436b26e15175249803c41e892a3f7b0da306
Nannapat <NannapatSrimartpirom@gmail.com>
2025-03-13 21:02:58 +0700

    Add cover image functionality to posts; update form and display logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -72,6 +72,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
+      params.require(:post).permit(:title, :content, :cover_image).merge(user_id: current_user.id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :tags, through: :post_tags
   belongs_to :user
+  has_one_attached :cover_image
+
   validates :title, presence: true
   validates :content, presence: true
 end
