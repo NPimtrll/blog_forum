967f535a863bc64dce3fb15763533bcc8bc2a2a4
Nannapat <NannapatSrimartpirom@gmail.com>
2025-03-20 09:24:59 +0700

    ปรับปรุงการจัดการปุ่มสลับในหน้าแก้ไขโปรไฟล์ผู้ใช้ โดยปรับตำแหน่งการแสดงผลของสไตล์ให้เหมาะสมยิ่งขึ้น และลบคอมเมนต์ที่ไม่จำเป็นในไฟล์ JavaScript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@rails/ujs"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-
-// TinyMCE
 import "tinymce"
