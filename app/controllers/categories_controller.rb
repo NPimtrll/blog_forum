a4d0951e6fc270e46acb2001cebddd15093bdf64
Nannapat <NannapatSrimartpirom@gmail.com>
2025-03-26 16:42:32 +0700

    ปรับปรุง CategoriesController โดยเพิ่มฟังก์ชันการแสดงหมวดหมู่และโพสต์ในหน้าแสดงหมวดหมู่ ปรับปรุง navbar เพื่อแสดงหมวดหมู่ทั้งหมด

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,54 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_category, only: [:show]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @posts = @category.posts.includes(:user, :comments, :likes).order(created_at: :desc)
+  end
 
   def new
     @category = Category.new
   end
 
   def create
+    Rails.logger.info "Creating new category with params: #{params.inspect}"
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to new_post_path, notice: "Category was successfully created."
+      Rails.logger.info "Category saved successfully: #{@category.inspect}"
+      respond_to do |format|
+        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.turbo_stream do
+          Rails.logger.info "Rendering turbo stream response"
+          render turbo_stream: turbo_stream.append("categories", partial: "posts/category_option", locals: { category: @category })
+        end
+      end
     else
-      render :new, status: :unprocessable_entity
+      Rails.logger.error "Failed to save category: #{@category.errors.full_messages}"
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream do
+          Rails.logger.info "Rendering error turbo stream response"
+          render turbo_stream: turbo_stream.update("category_error", partial: "shared/error_messages", locals: { object: @category })
+        end
+      end
     end
   end
 
   private
 
+  def set_category
+    @category = Category.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Category not found"
+    redirect_to root_path
+  end
+
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories, only: [ :new, :create ]
+  resources :categories, only: [ :new, :create, :show ]
   post "tinymce_assets", to: "tinymce_assets#create"
   devise_for :users
   resources :users, only: [ :show, :edit, :update ] do
