2b34fddd9bad036a8a05908b2b62ea656d5c0362
Nannapat <NannapatSrimartpirom@gmail.com>
2025-04-11 15:33:51 +0700

    Update application stylesheets to include Font Awesome, enhance JavaScript entry point with Rails UJS import, and update importmap for UJS. Adjust coverage report timestamps.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
-import "@rails/ujs"
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import "trix"
 import "@rails/actiontext"
 import "tinymce"
+
+// Import Rails UJS
+import Rails from "@rails/ujs"
+Rails.start()
