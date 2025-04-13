4ed2ab62a4aaf69ef47e94ef76172690135dad08
Nannapat <NannapatSrimartpirom@gmail.com>
2025-04-13 13:07:24 +0700

    Add sign-in success notice and display in posts index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar, :username ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar, :username ])
   end
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    posts_path
+  end
 end
