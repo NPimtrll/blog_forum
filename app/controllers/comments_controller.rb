ad115ff8a2fd77bb95452810a168bbd44775ae08
Nannapat <NannapatSrimartpirom@gmail.com>
2025-04-23 01:17:55 +0700

    Refactor comments controller and views for improved user experience and enhanced functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,10 +31,14 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     @comment.parent_id = params[:parent_id] if params[:parent_id].present?
 
-    if @comment.save
-      redirect_to post_path(@post, anchor: "comment-#{@comment.id}"), notice: "Comment was successfully created."
-    else
-      redirect_to post_path(@post), alert: "Failed to add comment."
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@post, anchor: "comment-#{@comment.id}"), notice: "Comment was successfully created." }
+        format.turbo_stream
+      else
+        format.html { redirect_to post_path(@post), alert: "Failed to add comment." }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", partial: "comments/form", locals: { post: @post, comment: @comment }) }
+      end
     end
   end
 
